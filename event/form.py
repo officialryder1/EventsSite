d4f054431fcd183b6ea2a65e1601e64b545cb305
victor <kennethvic07@gmail.com>
2022-07-22 23:58:17 +0100

    'updated'

--- event/form.py
+++ event/form.py
@@ -4,17 +4,17 @@ from django import forms
 from .models import Post, Category
 
 
-choices = Category.objects.all().values_list('name','name')
+# choices = Category.objects.all().values_list('name','name')
 
-choice_list = []
+# choice_list = []
 
-for item in choices:
-    choice_list.append(item)
+# for item in choices:
+#     choice_list.append(item)
 
-# try:
-#     cat_choices = Category.objects.all().values_list('name','name')
-# except (OperationalError, ProgrammingError) as e:
-#     cat_choices=[]
+try:
+    cat_choices = Category.objects.all().values_list('name','name')
+except (OperationalError, ProgrammingError) as e:
+    cat_choices=[]
 
 class AddPost(forms.ModelForm):
 
@@ -27,7 +27,7 @@ class AddPost(forms.ModelForm):
             'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Event Title'}),
             'author':forms.Select(attrs={'class':'form-control', 'placeholder':'Author'}),
             'description':forms.Textarea(attrs={'class':'form-control', 'placeholder':'Description'}),
-            'category':forms.Select(choices=choice_list ,attrs={'class':'form-control', 'placeholder':'Category'}),
+            'category':forms.Select(attrs={'class':'form-control', 'placeholder':'Category'}),
             'location':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Location'}),
             'date':forms.SelectDateWidget(attrs={'class':'form-control', 'placeholder':'Date'})
         }
