1b0f6bed26557c0ef46b37f9897c8531319047c8
victor <kennethvic07@gmail.com>
2022-07-22 11:17:21 +0100

    'updated'

--- event/models.py
+++ event/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
 class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -17,3 +11,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' --- ' + str(self.author)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
